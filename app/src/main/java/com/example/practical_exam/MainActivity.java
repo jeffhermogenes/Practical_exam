2e25c9466341396874d8e4671bd3204767e590b4
unknown <gerardjefferson.hermogenes.cics@ust.edu.ph>
2022-11-19 11:39:27 +0800

    User Interface

---
+++ app/src/main/java/com/example/practical_exam/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.practical_exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("HERMOGENES_GERARDJEFFERSON_D");
+    }
+}
